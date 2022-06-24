cb896363da11f52fdf31168a495767e01d505ce3
andobalazsandras <balazs.ando@codecool.com>
2022-06-24 10:39:38 +0200

    add flask basics tutorial

---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask, request, render_template, session, redirect, url_for, jsonify
+
+app = Flask(__name__)
+
+
+app.secret_key = 'hello'
+
+
+@app.route('/')
+def hello_world():
+    return 'ez egy get'
+
+
+@app.route('/', methods=['POST'])
+def hello_post():
+    return 'ez egy post'
+
+
+@app.route('/', methods=['PUT'])
+def hello_put():
+    return "ez egy put"
+
+
+@app.route('/without-redirect', methods=['GET', 'POST'])
+def post_without_redirect():
+    if request.method == 'POST':
+        data = session.get('data', [])
+        data.append(request.form.get('input'))
+        session['data'] = data
+    return render_template('form-with-session.html')
+
+
+@app.route('/with-redirect')
+def post_with_redirect_get():
+    return render_template('form-with-session.html')
+
+
+@app.route('/with-redirect', methods=['POST'])
+def post_with_redirect_post():
+    data = session.get('data', [])
+    data.append(request.form.get('input'))
+    session['data'] = data
+    return redirect(url_for('post_with_redirect_get'))
+
+
+@app.route('/custom-code')
+def route_with_401_code():
+    return "ez egy route 401-es http respone code-dal", 401
+
+
+@app.route('/json')
+def json_response():
+    json = {'key1': 'value1', 'key2': 'value2'}
+    return jsonify(json)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
